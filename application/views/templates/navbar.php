605225d55e5a2417d14101cbd894886aec519457
mwauragitonga <mwauragitonga12@gmail.com>
2021-07-26 14:34:36 +0300

    Login & Authentication & Sessions Working

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -5,7 +5,7 @@
 		<a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
 
 		<div class="nav-logo align-self-center">
-			<a class="navbar-brand" href="index.html"><img alt="logo" src="<?php echo base_url(); ?>assets/img/90x90.jpg"> <span class="navbar-brand-name">Naya Solutions</span></a>
+			<a class="navbar-brand" href="<?php echo base_url() ?>home"><img alt="logo" src="<?php echo base_url(); ?>assets/img/90x90.jpg"> <span class="navbar-brand-name">Naya Solutions</span></a>
 		</div>
 
 		<ul class="navbar-item flex-row mr-auto">
@@ -18,7 +18,7 @@
 					<div class="media">
 						<img src="assets/img/90x90.jpg" class="img-fluid" alt="admin-profile">
 						<div class="media-body align-self-center">
-							<h6><span>Hi,</span> Alan</h6>
+							<h6><span>Hi,</span> <?php echo $this->session->userdata("username") ?></h6>
 						</div>
 					</div>
 					<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
@@ -26,10 +26,10 @@
 				<div class="dropdown-menu position-absolute animated fadeInUp" aria-labelledby="user-profile-dropdown">
 					<div class="">
 						<div class="dropdown-item">
-							<a class="" href="user_profile.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> My Profile</a>
+							<a class="" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg> My Profile</a>
 						</div>
 						<div class="dropdown-item">
-							<a class="" href="auth_login.html"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg> Sign Out</a>
+							<a class="" href="<?php echo base_url() ?>unset"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg> Sign Out</a>
 						</div>
 					</div>
 				</div>
