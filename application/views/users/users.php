e585eff1f41acd22dcc72941f3c26c8c87ea05f9
mwauragitonga <mwauragitonga12@gmail.com>
2021-07-29 11:27:55 +0300

    Users Bug FIx

--- application/views/users/users.php
+++ application/views/users/users.php
@@ -57,7 +57,7 @@
 									$department = $staff->department;
 									$access_level = $staff->usergroup;
 									$date_created  = $staff->date_added;
-									$password = $staff->password;
+									$password ="12345";
 								?>
 									<a onclick="updateModal(<?php echo "'$staff_id'" . ',' ."'$name'" . ',' . "'$email'" .  ',' . "'$mobile'" . ',' . "'$department'"
 										. ',' . "'$access_level'" ?>)" class="bs-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="Edit"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-edit-2 p-1 br-6 mb-1"><path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z"></path></svg></a>
