6502f57e1aa239c29c46b987ba5ac53a07da2542
mwauragitonga <mwauragitonga12@gmail.com>
2021-08-02 16:11:14 +0300

    Session Expiry

--- application/views/templates/navbar.php
+++ application/views/templates/navbar.php
@@ -5,7 +5,7 @@
 		<a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>
 
 		<div class="nav-logo align-self-center">
-			<a class="navbar-brand" href="<?php echo base_url() ?>home"><img alt="logo" src="<?php echo base_url(); ?>assets/img/90x90.jpg"> <span class="navbar-brand-name">Naya Solutions</span></a>
+			<a class="navbar-brand" href="<?php echo base_url() ?>home"><img alt="logo" src="a<?php echo base_url(); ?>ssets/img/90x90.jpg"> <span class="navbar-brand-name">Naya Solutions</span></a>
 		</div>
 
 		<ul class="navbar-item flex-row mr-auto">
@@ -16,7 +16,7 @@
 			<li class="nav-item dropdown user-profile-dropdown order-lg-0 order-1">
 				<a href="javascript:void(0);" class="nav-link dropdown-toggle user" id="user-profile-dropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 					<div class="media">
-						<img src="assets/img/90x90.jpg" class="img-fluid" alt="admin-profile">
+						<img src="<?php echo base_url(); ?>assets/img/90x90.jpg" class="img-fluid" alt="admin-profile">
 						<div class="media-body align-self-center">
 							<h6><span>Hi,</span> <?php
 								$username = $this->session->userdata('username');
@@ -39,7 +39,6 @@
 						</div>
 					</div>
 				</div>
-
 			</li>
 		</ul>
 	</header>
