c27a43a7a2449cf50c8b795cb88bc42cd34d3f43
mwauragitonga <mwauragitonga12@gmail.com>
2021-08-06 11:06:15 +0300

    hware/sware line items formatting amounts

--- application/views/quotes/generateQuote.php
+++ application/views/quotes/generateQuote.php
@@ -1862,8 +1862,8 @@
 					'data-original-title="Edit">' +
 					'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 19h1.414l9.314-9.314-1.414-1.414L5 17.586V19zm16 2H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L9.243 19H21v2zM15.728 6.858l1.414 1.414 1.414-1.414-1.414-1.414-1.414 1.414z" fill="rgba(216,176,12,1)"/></svg>' +
 					'</a>'
-				tr += "<tr id='tr" + i + "'><td>" + (i + 1) + "</td><td id='td_desc'>" + description + "</td><td id='td_uom'>" + buying_price +
-						"</td><td id='td_quantity'>" + quantity + "</td><td id='td_rate'>" + selling_price + "</td><td id='td_remarks'>" + remarks + "</td>" + "<td >" + vat + "</td>" + "<td>" + edit_icon + "</td></tr>"
+				tr += "<tr id='tr" + i + "'><td>" + (i + 1) + "</td><td id='td_desc'>" + description + "</td><td id='td_uom'>" + parseFloat(buying_price).toLocaleString('en-US', {minimumFractionDigits: 2,maximumFractionDigits: 2,}) +
+						"</td><td id='td_quantity'>" + quantity + "</td><td id='td_rate'>" + selling_price.toLocaleString('en-US', {minimumFractionDigits: 2,maximumFractionDigits: 2,}) + "</td><td id='td_remarks'>" + remarks + "</td>" + "<td >" + vat + "</td>" + "<td>" + edit_icon + "</td></tr>"
 			}
 			tbody.innerHTML = tr;
 			/*	4. Reset && hide modal  */
@@ -1941,8 +1941,8 @@
 					'data-original-title="Edit">' +
 					'<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"/><path d="M5 19h1.414l9.314-9.314-1.414-1.414L5 17.586V19zm16 2H3v-4.243L16.435 3.322a1 1 0 0 1 1.414 0l2.829 2.829a1 1 0 0 1 0 1.414L9.243 19H21v2zM15.728 6.858l1.414 1.414 1.414-1.414-1.414-1.414-1.414 1.414z" fill="rgba(216,176,12,1)"/></svg>' +
 					'</a>'
-				tr += "<tr id='tr" + i + "'><td>" + (i + 1) + "</td><td id='td_desc'>" + description + "</td><td id='td_uom'>" + buying_price +
-					"</td><td id='td_quantity'>" + quantity + "</td><td id='td_rate'>" + selling_price + "</td><td id='td_remarks'>" + remarks + "</td>" + "<td >" + vat + "</td>" + "<td>" + edit_icon + "</td></tr>"
+				tr += "<tr id='tr" + i + "'><td>" + (i + 1) + "</td><td id='td_desc'>" + description + "</td><td id='td_uom'>" + buying_price.toLocaleString('en-US', { minimumFractionDigits: 2,maximumFractionDigits: 2,}) +
+					"</td><td id='td_quantity'>" + quantity + "</td><td id='td_rate'>" + selling_price.toLocaleString('en-US', {minimumFractionDigits: 2,maximumFractionDigits: 2,}) + "</td><td id='td_remarks'>" + remarks + "</td>" + "<td >" + vat + "</td>" + "<td>" + edit_icon + "</td></tr>"
 			}
 			tbody.innerHTML = tr;
 			/*	4. Reset && hide modal  */
